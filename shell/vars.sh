96218ec6ac52c90476105697fbe86dd8a05533eb
Lilly Dinh-le <lilly.dinhle@prodigygame.com>
2020-02-11 20:16:49 -0500

    Add zsh-nvm plugin to auto switch node versions

--- shell/vars.sh
+++ shell/vars.sh
@@ -5,3 +5,11 @@ export DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 export LOCAL_CONFIG_DIRECTORY="${DOTFILES_DIRECTORY}/local"
 export ZSH_CONFIG_DIRECTORY="${DOTFILES_DIRECTORY}/zsh"
 export ZDOTDIR="${ZSH_CONFIG_DIRECTORY}"
+
+# ----------------------------------------------------------------------------
+# zsh-nvm
+# ----------------------------------------------------------------------------
+
+# Tell zsh-nvm to auto switch node versions based on a `.nvmrc` file in the
+# target directory
+export NVM_AUTO_USE=true
--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -1,2 +1,4 @@
 zplugin ice lucid wait as'program' pick'git-coauthor'
 zplugin light zkhvan/git-coauthor
+
+zplugin light lukechilds/zsh-nvm.git
