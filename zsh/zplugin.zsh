c1f60a9ae0bace78c9348fcc49ae10fb71ec54d6
Lilly Dinh-le <lilly.dinhle@prodigygame.com>
2020-02-11 20:46:04 -0500

    Add plugin to jump to previously visited directories

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -2,3 +2,6 @@ zplugin ice lucid wait as'program' pick'git-coauthor'
 zplugin light zkhvan/git-coauthor
 
 zplugin light lukechilds/zsh-nvm.git
+
+zplugin ice lucid nocompletions
+zplugin light 'skywind3000/z.lua'
